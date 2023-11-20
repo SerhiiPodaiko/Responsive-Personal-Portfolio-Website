129d104c3378a1540476369ba253cd59d4f556f2
SerhiiPodaiko <podajko1997@gmail.com>
2023-11-20 16:09:01 +0200

    Refactoring directories

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const { NEXT_MY_FULL_NAME = process.env.NEXT_MY_FULL_NAME } =
+    process.env
+
+const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')]
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
+        })
+
+        return config
+    },
+    env: {
+        NEXT_MY_FULL_NAME
+    },
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'i.dummyjson.com',
+    //             port: '',
+    //             pathname: '/data/products/**'
+    //         }
+    //     ]
+    // }
+}
 
 module.exports = nextConfig
